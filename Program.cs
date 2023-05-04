79d1e2a70c7c19b5dfd76c6f986de9c9f8423f31
Federico Cau <116791048+fedecauwedy@users.noreply.github.com>
2023-05-04 09:11:05 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SQLite;
+
+
+Console.WriteLine("Hello, World!");
+
+// Connessione al db
+SQLiteConnection cn1 = new SQLiteConnection("chinook.db");
+var tblArtists = cn1.Query<Artist>("select * from Artists");
+Console.WriteLine($" in questa tabella ci sono {tblArtists.Count} record!");
+
+
+//fine del main.... inizio delle dichiarazioni
+public class Artist
+{
+    public int ArtistId{get;set;}
+    public string Name{get;set;}
+}
